90333d976c2d687926b34411291f329c47beaf0b
Amesul <samuelstroesser@icloud.com>
2024-05-13 15:04:22 +0200

    Upgrade Router file to V1 (auth part only)

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,37 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\EventController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\VenueController;
+use App\Http\Controllers\Artist\EventUserController;
+use App\Http\Controllers\Artist\PerformanceController;
+use App\Http\Controllers\SettingsController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+
+// ARTIST ROUTER
+Route::domain('artiste.' . config('app.domain'))->name('artist.')->middleware('can:perform')->group(function () {
+    Route::view('/directory', 'artist.directory', ['users' => User::all()]);
+    Route::resource('performances', PerformanceController::class);
+    Route::resource('events', EventUserController::class);
+});
+
+// ADMIN ROUTER
+Route::domain('admin.' . config('app.domain'))->name('admin.')->middleware('can:administrate')->group(function () {
+    Route::resource('events', EventController::class);
+    Route::resource('venues', VenueController::class);
+    Route::resource('users', UserController::class)->only(['index', 'update', 'destroy']);
+});
+
+// ACCOUNT ROUTER
+Route::domain('account.' . config('app.domain'))->name('.account')->middleware('auth')->group(function () {
+    Route::get('/profile', [SettingsController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [SettingsController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [SettingsController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [UserController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [UserController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [UserController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
