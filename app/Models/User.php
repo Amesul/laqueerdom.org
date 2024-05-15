1a15e0417db4d6588b58da1727714bc9305734d9
Amesul <samuelstroesser@icloud.com>
2024-05-15 19:02:34 +0200

    Add 'username' to fillable fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Propaganistas\LaravelPhone\Casts\E164PhoneNumberCast;
 
 class User extends Authenticatable
 {
@@ -18,7 +20,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = ['name', 'email', 'password', 'phone', 'phone_country'];
+    protected $fillable = ['name', 'username', 'email', 'password', 'phone', 'phone_country', 'privacy'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -34,7 +36,11 @@ class User extends Authenticatable
      */
     protected function casts(): array
     {
-        return ['email_verified_at' => 'datetime', 'password' => 'hashed',];
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'phone' => E164PhoneNumberCast::class,
+        ];
     }
 
     public function roles(): BelongsToMany
@@ -50,6 +56,11 @@ class User extends Authenticatable
     public function documents(): HasMany
     {
         return $this->hasMany(Document::class);
+    }
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
     }
 
     public function hasRole(string $role): bool
