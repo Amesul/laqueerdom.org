5bfc3dd05cf24536c95c06da8669ef1c903ba292
Amesul <samuelstroesser@icloud.com>
2024-05-15 19:02:59 +0200

    Add public Routes and some new Routes for Auth users

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Admin\VenueController;
 use App\Http\Controllers\Artist\EventUserController;
 use App\Http\Controllers\Artist\PerformanceController;
 use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\PrivacyController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\ShowController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +19,8 @@ Route::domain('artiste.' . config('app.domain'))->name('artist.')->middleware('a
         Route::view('/dashboard', 'artist.dashboard')->name('dashboard');
         Route::view('/directory', 'artist.directory', ['users' => User::all()]);
         Route::resource('performances', PerformanceController::class);
-        Route::resource('events', EventUserController::class);
+        Route::resource('events', EventUserController::class)->only('index', 'show');
+        Route::view('/directory', 'artist.directory', ['users' => User::all()])->name('directory');
     });
 });
 
@@ -27,13 +31,14 @@ Route::domain('admin.' . config('app.domain'))->middleware('auth')->name('admin.
     Route::middleware(['can:edit', 'can:administrate'])->group(function () {
         Route::resource('events', EventController::class)->only('index', 'edit', 'update');
         Route::resource('venues', VenueController::class)->only('index', 'edit', 'update');
-        Route::resource('documents', DocumentController::class);
+        Route::resource('shows', ShowController::class)->only('index', 'edit', 'update');
+        Route::resource('documents', DocumentController::class)->only('index', 'edit', 'update');
         Route::resource('users', UserController::class)->only('index');
     });
 
     Route::middleware('can:administrate')->group(function () {
-        Route::resource('events', EventController::class)->except('index', 'edit', 'update');
-        Route::resource('venues', VenueController::class)->except('index', 'edit', 'update');
+        Route::resource('events', EventController::class)->except('show', 'index', 'edit', 'update');
+        Route::resource('venues', VenueController::class)->except('show', 'index', 'edit', 'update');
         Route::resource('users', UserController::class)->only(['update', 'destroy']);
     });
 });
@@ -41,13 +46,25 @@ Route::domain('admin.' . config('app.domain'))->middleware('auth')->name('admin.
 // ACCOUNT ROUTER
 Route::domain(config('app.domain'))->middleware('auth')->group(function () {
     Route::get('/profile', [SettingsController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [SettingsController::class, 'update'])->name('profile.update');
+    Route::patch('/user', [SettingsController::class, 'update'])->name('user.update');
+    Route::patch('/profile', ProfileController::class)->name('profile.update');
+    Route::patch('/privacy', PrivacyController::class)->name('privacy.update');
     Route::delete('/profile', [SettingsController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+// PUBLIC ROUTER
 Route::domain(config('app.domain'))->group(function () {
-    Route::view('/', 'welcome');
+    Route::view('/', 'public.welcome')->name('home');
+    Route::view('/agenda', 'public.agenda')->name('agenda');
+    Route::view('/about', 'public.about')->name('about');
+    Route::view('/about/team', 'public.team')->name('about.team');
+    Route::view('/about/artists', 'public.artists-index')->name('about.artists');
+    Route::view('/about/artists/{user:username}', 'public.artists-show')->name('artist.show');
+    Route::view('/about/legal', 'public.legal')->name('legal');
+    Route::view('/contact', 'public.contact')->name('contact');
 });
+
 
 require __DIR__ . '/auth.php';
 
