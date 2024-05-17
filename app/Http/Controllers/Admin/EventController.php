8e67d0d5198065179152b27764dcc405bf0dd9e4
Amesul <samuelstroesser@icloud.com>
2024-05-17 10:49:41 +0200

    Update methods according to the app architecture

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -4,45 +4,88 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
+use App\Models\Venue;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
 
 class EventController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|Factory|\Illuminate\Contracts\View\View|Application|View
+     */
     public function index()
     {
         return view('admin.event.index', [
-            'events' => Event::with('venue')->orderBy('date')->get(),
+            'events' => Event::with('venue')->orderBy('date', 'desc')->get(),
         ]);
     }
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|Factory|\Illuminate\Contracts\View\View|Application|View
+     */
     public function create()
     {
-        return view('admin.event.create');
+        return view('admin.event.create', [
+            'venues' => Venue::orderBy('name')->get(),
+        ]);
     }
 
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function store(Request $request)
     {
         $attributes = $this->validateRequest($request);
 
         Event::create($attributes);
 
-        return back()->with('success', 'Événement créé avec succès.');
+        return redirect(route('admin.events.index'))->with('success', 'Événement créé avec succès.');
     }
 
+    /**
+     * @param Event $event
+     * @return \Illuminate\Contracts\Foundation\Application|Factory|\Illuminate\Contracts\View\View|Application|View
+     */
     public function edit(Event $event)
     {
-        return view('admin.event.edit', ['event' => $event]);
+        return view('admin.event.edit', [
+            'event' => $event,
+            'venues' => Venue::orderBy('name')->get(),
+        ]);
     }
 
+    /**
+     * @param Request $request
+     * @param Event $event
+     * @return RedirectResponse
+     */
     public function update(Request $request, Event $event)
     {
         $attributes = $this->validateRequest($request);
+
+        if ($request->hasFile('thumbnail')) {
+            Storage::delete(substr($event->thumbnail, 9));
+        }
+
+        // Prevent update on type column
+        $attributes['type'] = $event->type;
 
         $event->update($attributes);
         return back()->with('success', 'Événement modifié avec succès.');
 
     }
 
+    /**
+     * @param Event $event
+     * @return \Illuminate\Contracts\Foundation\Application|Application|RedirectResponse|Redirector
+     */
     public function destroy(Event $event)
     {
         $event->delete();
@@ -55,14 +98,23 @@ class EventController extends Controller
      */
     public function validateRequest(Request $request): array
     {
-        return $request->validate([
-            'venue_id' => ['required', 'exists:venues'],
+        $attributes = $request->validate([
+            'venue_id' => ['required', 'exists:venues,id'],
             'title' => ['required'],
             'description' => ['nullable', 'string'],
-            'date' => ['required', 'datetime'],
+            'date' => ['required', 'date'],
             'price' => ['nullable', 'integer'],
             'thumbnail' => ['nullable'],
             'type' => ['required', 'string'],
         ]);
+
+        $attributes['slug'] = Str::slug($attributes['title']);
+
+        if ($request->hasFile('thumbnail')) {
+            $thumbnailPath = $request->file('thumbnail')->store('thumbnails');
+            $attributes['thumbnail'] = $thumbnailPath;
+        }
+
+        return $attributes;
     }
 }
--- app/Http/Controllers/Admin/VenueController.php
+++ app/Http/Controllers/Admin/VenueController.php
@@ -4,54 +4,88 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Venue;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\View\View;
 
 class VenueController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|Factory|\Illuminate\Contracts\View\View|Application|View
+     * Show all the records in the venues table
+     */
     public function index()
     {
         return view('admin.venue.index', ['venues' => Venue::all()]);
     }
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|Factory|\Illuminate\Contracts\View\View|Application|View
+     * Return the view to create a Venue
+     */
+    public function create()
+    {
+        return view('admin.venue.create');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|Application|RedirectResponse|Redirector
+     * Create a record in the venues table
+     */
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'name' => ['required'],
-            'address' => ['required'],
-            'address2' => ['nullable'],
-            'zip_code' => ['required'],
-            'city' => ['required'],
-            'country' => ['required'],
-        ]);
+        $attributes = $this->validateRequest($request);
 
-        return Venue::create($data);
+        Venue::create($attributes);
+
+        return redirect(route('admin.venue.index'))->with('success', 'Structures créé avec succès.');
     }
 
-    public function show(Venue $venue)
-    {
-        return $venue;
-    }
-
+    /**
+     * @param Request $request
+     * @param Venue $venue
+     * @return RedirectResponse
+     * Update specified record in the venues table
+     */
     public function update(Request $request, Venue $venue)
     {
-        $data = $request->validate([
-            'name' => ['required'],
-            'address' => ['required'],
-            'address2' => ['nullable'],
-            'zip_code' => ['required'],
-            'city' => ['required'],
-            'country' => ['required'],
-        ]);
+        $attributes = $this->validateRequest($request);
 
-        $venue->update($data);
+        $venue->update($attributes);
 
-        return $venue;
+        return back()->with('success', 'Structure modifiée avec succès.');
     }
 
+    /**
+     * @param Venue $venue
+     * @return \Illuminate\Contracts\Foundation\Application|Application|RedirectResponse|Redirector
+     * Delete specified record in the venues table
+     */
     public function destroy(Venue $venue)
     {
         $venue->delete();
 
-        return response()->json();
+        return redirect(route('admin.venues.index'))->with('danger', 'Structure supprimée');
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     * Validate Venue form request
+     */
+    public function validateRequest(Request $request): array
+    {
+        return $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'address2' => ['nullable', 'string', 'max:255'],
+            'zip_code' => ['required', 'string', 'max:16'],
+            'city' => ['required', 'string', 'max:255'],
+            'country' => ['required', 'string', 'max:255'],
+        ]);
     }
 }
