4dfa85bb8f3c4e1f89d8e605e6a2990ff686478f
Amesul <samuelstroesser@icloud.com>
2024-05-21 22:23:31 +0200

    Create create and destroy methods

---
+++ app/Http/Controllers/Admin/CommentController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'message' => ['required', 'max:300', 'string'],
+            'user_id' => ['required', 'exists:users,id'],
+            'document_id' => ['required', 'exists:documents,id'],
+        ]);
+
+        Comment::create($attributes);
+
+        return back()->with('success', 'Commentaire posté.');
+    }
+
+    /**
+     * @param Comment $comment
+     * @return RedirectResponse
+     * Delete specified comment
+     */
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return back()->with('danger', 'Commentaire supprimé.');
+    }
+}
