41fbd12f21f96705aa753d80f7297ee09942cf52
Amesul <samuelstroesser@icloud.com>
2024-05-21 22:27:08 +0200

    Support a new type of WYSIWYG, with more options for Document edition

--- resources/views/components/tinymce/config.blade.php
+++ resources/views/components/tinymce/config.blade.php
@@ -6,6 +6,17 @@
         plugins: 'code link wordcount',
         content_style: '@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"); body { font-family: "Quicksand", sans-serif; font-optical-sizing: auto; }',
         menubar: false,
+        language: 'fr_FR',
         toolbar: 'undo redo | bold italic | link code | wordcount'
+    });
+
+    tinymce.init({
+        selector: '.advanced-wysiwyg',
+        plugins: 'code lists link wordcount autosave searchreplace help autolink preview',
+        content_style: '@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"); body { font-family: "Quicksand", sans-serif; font-optical-sizing: auto; }',
+        autosave_interval: '20s',
+        menubar: 'file edit view tools help',
+        language: 'fr_FR',
+        toolbar: 'undo redo | blocks | bold italic | numlist bullist | link | searchreplace wordcount | restoredraft '
     });
 </script>
