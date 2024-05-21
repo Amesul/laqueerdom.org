250fab524ea6a93b7ccf901d31f86fc36b08f4c1
Amesul <samuelstroesser@icloud.com>
2024-05-21 22:26:33 +0200

    Add margin top

--- resources/views/components/tinymce/editor.blade.php
+++ resources/views/components/tinymce/editor.blade.php
@@ -1,4 +1,4 @@
-<div class="shadow-md">
+<div class="shadow-md mt-1">
     <textarea {{ $attributes->merge([
     'class' => 'focus-visible:outline-accent focus:ring ring-accent']) }}>{{ $slot }}</textarea>
 </div>
