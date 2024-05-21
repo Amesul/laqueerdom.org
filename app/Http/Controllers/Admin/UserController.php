881f8d84e4324abe70dc343b310a4ff5b352dcf2
Amesul <samuelstroesser@icloud.com>
2024-05-21 16:31:25 +0200

    Create update method

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,28 +4,47 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|Factory|\Illuminate\Contracts\View\View|Application|View
+     */
     public function index()
     {
-        return view('admin.user.index', ['users' => User::all()]);
+        return view('admin.user.index', ['users' => User::with(['profile', 'roles'])->orderBy('username')->get()]);
     }
 
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return RedirectResponse
+     */
     public function update(Request $request, User $user)
     {
-        $user->roles()->attach($request->role);
-        return back()->with('success', 'Rôle ajouté avec succès.');
-    }
+        $attributes = $request->validate([
+            'add_roles' => 'nullable',
+            'remove_role' => 'nullable',
+        ]);
 
-    public function destroy(Request $request, User $user)
-    {
-        $request->validate(['confirmation' => 'required']);
-        if (request('confirmation') == "Supprimer " . $user->username) {
-            $user->delete();
-            return back()->with('danger', 'Utilisateur supprimé');
+        if (isset($attributes['add_roles'])) {
+            $roles = explode(',', $attributes['add_roles']);
+
+            foreach ($roles as $role) {
+                $user->roles()->attach($role);
+            }
+
+            return back()->with('success', 'Rôle(s) ajouté(s) avec succès.');
+        } elseif (isset($attributes['remove_role'])) {
+            $user->roles()->detach($attributes['remove_role']);
+
+            return back()->with('success', 'Rôle retiré avec succès.');
         }
-        return back()->with('info', 'Échec.');
+        return back()->with('danger', 'Échec lors de la modification des rôles.');
     }
 }
