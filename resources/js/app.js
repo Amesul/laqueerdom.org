7e572d490f3ef097f1f5d473fc65da0b23faf5c3
Amesul <samuelstroesser@icloud.com>
2024-05-24 14:10:45 +0200

    Import new AlpineJS plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+import persist from '@alpinejs/persist';
+import sort from '@alpinejs/sort';
+
+Alpine.plugin(sort);
+Alpine.plugin(focus);
+Alpine.plugin(persist);
 
 window.Alpine = Alpine;
-
 Alpine.start();
