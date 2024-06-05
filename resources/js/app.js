07c28d7f791d39601853f9d0c01d0bfe4b3b3820
Amesul <samuelstroesser@icloud.com>
2024-06-05 19:25:50 +0200

    Add Alpine Collapse plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,12 @@ import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import persist from '@alpinejs/persist';
 import sort from '@alpinejs/sort';
+import collapse from '@alpinejs/collapse'
 
 Alpine.plugin(sort);
 Alpine.plugin(focus);
 Alpine.plugin(persist);
+Alpine.plugin(collapse)
 
 window.Alpine = Alpine;
 Alpine.start();
